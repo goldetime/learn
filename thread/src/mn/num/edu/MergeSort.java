5547ce677575c7b751ee2fc27837e9b6143c5558
Ochirsum <ochirsum74@gmail.com>
2020-08-04 17:43:11 +0800

    refactor

--- thread/src/mn/num/edu/MergeSort.java
+++ thread/src/mn/num/edu/MergeSort.java
@@ -43,7 +43,7 @@ public class MergeSort {
   public static void main(String[] args) {
     int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
     mergeSort(list);
-    for (int i = 0; i < list.length; i++)
-      System.out.print(list[i] + " ");
+    for (int value : list) 
+      System.out.print(value + " ");
   }
 }
